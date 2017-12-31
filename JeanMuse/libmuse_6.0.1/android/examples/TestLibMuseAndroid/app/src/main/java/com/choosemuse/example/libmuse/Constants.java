b42e58118a88c9fdf6215455f52c8832b42df941
jLouis <jeandlouis1@gmail.com>
2017-12-30 19:09:15 -0500

    fixed untracked files

--- JeanMuse/libmuse_6.0.1/android/examples/TestLibMuseAndroid/app/src/main/java/com/choosemuse/example/libmuse/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.choosemuse.example.libmuse;
-
-public class Constants {
-    //replace with your ip:portserver is listening on
-    public static final String CHAT_SERVER_URL = "http::3000";//http://ipadd:port
-}
